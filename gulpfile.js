9dc46c414d320a07929f2543a22d1a0ac24fad85
Balakumar95 <meganathankrishnan85@gmail.com>
2019-07-21 23:08:49 +0530

    Basic theme implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,35 @@
-var $ =require('gulp-load-plugins')();
-const { dest, src } = require('gulp');
+var $ = require('gulp-load-plugins')();
+const { dest, src, watch } = require('gulp');
 const rename = require('gulp-rename');
+const fs = require('fs');
 
 
-function style(cb) {
-    src('src/assets/scss/*.scss')
+function style(themeName) {
+    return src('src/assets/scss/*.scss')
+        .pipe($.replace('themes/', 'themes/' + themeName + '.scss'))
         .pipe($.sass({
             outputStyle: 'expanded',
             errLogToConsole: true,
             includePaths: ['node_modules/bootstrap/scss']
         }))
-        .pipe(rename('styles.css'))
+        .pipe(rename(themeName + '.css'))
         .pipe(dest('src/assets/css/'))
-    cb()
+       
 }
- 
-exports.style = style
+
+function styles(cb) {
+    console.log('CSS has changed');
+    const themes = fs.readdirSync('src/assets/scss/themes');
+    themes.forEach((theme) => {
+        const themeName = theme.split('.scss')[0];
+        if (themeName) {
+            style(themeName);
+        }
+    });
+    watch('./**/*.scss').on('change', () => {
+        styles(cb);
+    })
+    cb();
+}
+
+exports.styles = styles;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-pet-one-UI';
+
+  ngOnInit() {
+    const link =  document.querySelector('link#stylesheet');
+    link['href'] = 'assets/css/light.css';
+  }
 }
